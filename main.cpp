e0befeb24137400952eb3f0efdfc4570ceefc3c9
koreasunoo <koreasunoo@dgist.ac.kr>
2023-10-29 01:47:31 +0900

    ATM 클래스 일부 구현

---
+++ main.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <algorithm>
+#include <string.h>
+#include <vector> //쓸 지는 모르겠음
+
+using namespace std;
+
+//Forward declaration
+class ATM;
+
+//Classes declaration
+class ATM {
+private:
+    //ATM 선언 때 선언되는 변수들
+    bool isbilingual; //isbilingual은 이 atm이 두 가지의 언어를 지원하는지 알려줌 -> false면 영어만 언어 지원 (REQ 1.3)
+    bool isSingle; //isSingle은 이 atm이 하나의 은행만 가능한지 두 가지 다 가능한지 알려줌 -> true이면 한 개만 됨 (REQ 1.2)
+
+    const int NonPrimaryDepositFee = 1000; //다른 은행 예금 수수료 (REQ 1.8)
+    const int PrimaryWithdrawalFee = 1000; //같은 은행 출금 수수료 (REQ 1.8)
+    const int NonPrimaryWithDrawalFee = 2000; //다른 은행 출금 수수료 (REQ 1.8)
+    const int PrimarytoPrimaryFee = 2000; // 같은 은행끼리 송금 수수료 (REQ 1.8)
+    const int NonPrimarytoPrimaryFee = 3000; //같은, 다른 은행끼리 송금 수수료 (REQ 1.8)
+    const int NonPrimarytoNonPrimaryFee = 4000; // 다른 은행끼리 송금 수수료 (REQ 1.8)
+    const int CashTransferFee = 5000; //무통장 송금 수수료 (REQ 1.8)
+    
+    int AmountOfCash;
+
+    //ATM 호출 때 선언되는 변수들
+    int language_type; // 1이면 영어, 2이면 한국어 (REQ 1.3)
+
+
+public:
+    ATM(const bool language_option, bool ATM_type, int cash);
+
+    bool get_language(); // true면 영어라는 뜻, false면 한국어라는 뜻
+    bool get_ATM_type(); // isSinlge을 반환
+
+};
+ATM::ATM(const bool language_option, bool ATM_type, int cash){
+    isbilingual = language_option;
+    isSingle = ATM_type;
+    AmountOfCash = cash;
+}
+bool ATM::get_language(){
+    if(!isbilingual) return true; // unilingual이면 영어만 지원함
+    if(language_type == 1) return true;
+    else if(language_type == 2) return false;
+}
+bool ATM::get_ATM_type(){
+    return isSingle;
+}
+
+
+int main(){
+
+    return 0;
+}
