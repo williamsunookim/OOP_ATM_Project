29e403b2fb6ebf55e2737c9341f626a54e1fb51d
williamsunookim <126538720+williamsunookim@users.noreply.github.com>
2023-10-30 12:56:43 +0000

    input modified

--- main.cpp
+++ main.cpp
@@ -169,11 +169,11 @@ int main(){
     //ATM input
     vector<ATM*> ATM_list;
     cout<<"This is ATM input section. If you're done, input 0 or \"zero\"\n";
-    int i = 1;
+    int index = 1;
     while(1){
-        cout<<"-----"<<i<<"th ATM info-----\n";
+        cout<<"-----"<<index<<"th ATM info-----\n";
         //language_option(Unilingual/Bilingual) input
-        cout<<"ATM Type(Unilingual/Bilingual): ";
+        cout<<"ATM language option(Unilingual/Bilingual): ";
         string language_option;
         cin>>language_option;
         bool IsBilingual;
@@ -182,7 +182,7 @@ int main(){
         else IsBilingual = false;
         cout<<"This ATM will be "<<language_option<<'\n';
         //Bank input
-        cout<<"Current banks list:";
+        cout<<"Current banks list:\n";
         for(int i = 0; i<Bank_list.size(); i++){
             cout<<i+1<<". "<<Bank_list[i]->get_bank_name();
             cout<<"\n";
@@ -190,22 +190,23 @@ int main(){
         cout<<"Which Bank does this ATM use? Input the number of Bank: ";
         int bank_index;
         cin>>bank_index;
-        cout<<"You chose "<<Bank_list[bank_index-1]<<'\n';
+        cout<<"You chose "<<Bank_list[bank_index-1]->get_bank_name()<<'\n';
         //cash input
         cout<<"How much cash will this ATM have: ";
         long long cash;
         cin>>cash;
-        cout<<"This ATM will have "<<cash<<"Won\n";
+        cout<<"This ATM will have "<<cash<<" Won\n";
         //bank_type(Single/Multiple) input
-        cout<<"What kind of type will this ATM be(Single/Multiple)";
+        cout<<"ATM type(Single/Multiple): ";
         string bank_type;
         cin>>bank_type;
         bool IsSingle;
         if(bank_type[0] == 'S' || bank_type[0] == 's') IsSingle = true;
         else IsSingle = false;
-        cout<<"This ATM will be"<<bank_type<<'\n';
+        cout<<"This ATM will be "<<bank_type<<'\n';
         // input
         ATM_list.push_back(new ATM(IsBilingual, Bank_list[bank_index-1], cash, IsSingle));
+        index++;
     }
     //Account input
 
